b0e18f16309e1eecf7114466caaf36ed22423e21
Julian Blagis <julianbgs96@gmail.com>
2020-09-23 14:26:31 +0200

    finish

--- src/layout/header/index.js
+++ src/layout/header/index.js
@@ -4,21 +4,17 @@ export default () => {
   $(document).on('click', '#header-trigger', function (event) {
     event.preventDefault();
     const $menu = $('.header__mobile-menu');
-    const $list = $('.header__list--mobile');
-    const $trigger = $('#header-trigger');
     const $body = $('body')
-    $menu.animate({width:'toggle'}, 350, () => {
-      if ($trigger.hasClass('js-trigger-active')) {
-        $list.css('opacity', 1)
-      }
-    });
     if ($(this).hasClass('js-trigger-active')) {
       $(this).removeClass('js-trigger-active');
-      $list.css('opacity', 0)
+      $menu.removeClass('open');
+
       $body.css('overflow', 'auto');
     } else {
+
       $(this).addClass('js-trigger-active');
       $body.css('overflow', 'hidden');
+      $menu.addClass('open');
     }
     return false;
   });
