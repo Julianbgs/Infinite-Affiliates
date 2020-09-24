7d77faf64a7bf85a9f56b0b63d94c1a6d0eba1ea
Julian Blagis <julianbgs96@gmail.com>
2020-09-24 13:04:47 +0200

    fixes

--- src/layout/header/index.js
+++ src/layout/header/index.js
@@ -2,27 +2,35 @@ import './header.sass';
 
 export default () => {
   const $link = $('.header__link');
+  const $overlay = $('.header__mobile-menu-overlay');
+  const $menu = $('.header__mobile-menu');
+  const $trigger = $('#header-trigger');
+  const $body = $('body')
   $link.on('click', function (event) {
     event.preventDefault();
     $('html, body').animate({
       scrollTop: $($.attr(this, 'href')).offset().top
     }, 700);
+    setTimeout(() => {
+      $menu.removeClass('open');
+      $overlay.fadeOut();
+      $trigger.removeClass('js-trigger-active');
+      $body.css('overflow', 'auto');
+    }, 500)
   });
 
   $(document).on('click', '#header-trigger', function (event) {
     event.preventDefault();
-    const $menu = $('.header__mobile-menu');
-    const $body = $('body')
     if ($(this).hasClass('js-trigger-active')) {
       $(this).removeClass('js-trigger-active');
       $menu.removeClass('open');
-
       $body.css('overflow', 'auto');
+      $overlay.fadeOut();
     } else {
-
       $(this).addClass('js-trigger-active');
       $body.css('overflow', 'hidden');
       $menu.addClass('open');
+      $overlay.fadeIn();
     }
     return false;
   });
