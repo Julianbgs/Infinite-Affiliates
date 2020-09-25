68fb2de555fdda850c894c7abbb39a8d28d7c24c
Julian Blagis <julianbgs96@gmail.com>
2020-09-25 13:02:38 +0200

    add form

--- src/pages/home/callback/index.js
+++ src/pages/home/callback/index.js
@@ -6,16 +6,23 @@ export default () => {
     return re.test(email);
   }
 
-  function validate() {
+  function validate(e) {
+    e.preventDefault();
     const $resultEmail = $("#resultEmail");
     const $resultName = $("#resultName");
-    const email = $("#email").val();
+    const $resultDescr = $('#resultDescr');
+    const $email = $("#email").val();
     const $name = $('#name').val();
+    const $descr = $('#descr').val();
+    let validEmail = false;
+    let validName = false;
+    let validDescr = false;
     $resultEmail.text("");
 
-    if (validateEmail(email)) {
+    if (validateEmail($email)) {
+      validEmail = true;
     } else {
-      $resultEmail.text(email + " is not valid");
+      $resultEmail.text($email + " is not valid");
       $resultEmail.css("color", "red");
     }
 
@@ -23,7 +30,56 @@ export default () => {
       $resultName.text('Name must be not empty');
       $resultName.css("color", "red");
     } else {
+      validName = true;
       $resultName.text('');
+    }
+
+    if ($descr.length < 1) {
+      $resultDescr.text('Text must be not empty');
+      $resultDescr.css("color", "red");
+    } else {
+      validDescr = true;
+      $resultDescr.text('');
+    }
+
+    if (validName && validEmail && validDescr) {
+      let $modal = $('.callback__modal');
+      let $text = $('.callback__modal-title');
+      let $close = $('.callback__close');
+      let $body = $('body');
+      let bodyReq = {
+         username: $name,
+         email: $email,
+         text: $descr
+      }
+      const json = JSON.stringify(bodyReq);
+      $.ajax({
+        url: 'http://api.infiniteaffiliates.co/seller-site/contact-us/',
+        type: "POST",
+        data: json,
+        success: (resp) => {
+          $modal.css('display', 'flex')
+          $text.text('Success send');
+          $text.css('color', 'green');
+          $body.css('overflow', 'hidden');
+          $close.on('click', () => {
+            $modal.hide();
+            $body.css('overflow', 'auto');
+          })
+        },
+        error: () => {
+          $modal.css('display', 'flex')
+          $text.text('Fail send');
+          $text.css('color', 'red');
+          $body.css('overflow', 'hidden');
+          $close.on('click', () => {
+            $modal.hide();
+            $body.css('overflow', 'auto');
+          })
+        },
+        contentType:"application/json; charset=utf-8",
+        dataType:"json",
+      });
     }
 
     return false;
